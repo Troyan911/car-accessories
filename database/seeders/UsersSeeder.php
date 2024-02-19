88146b55db39fefe55d9961a551060a8424baf88
troyan911 <troyan911@gmail.com>
2024-02-19 03:46:40 +0200

    optimize use

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,14 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Enums\Roles;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UsersSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      */
