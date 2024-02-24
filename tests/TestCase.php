9a42cac3e669b76cfd884751afc506ff38d6bc3d
troyan911 <troyan911@gmail.com>
2024-02-24 13:03:34 +0200

    add tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Enums\Roles;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function getUser(Roles $role): User
+    {
+        return User::role($role->value)->firstOrFail();
+    }
+
+    protected function actingAsRole(Roles $role) {
+        return $this->actingAs($this->getUser($role));
+    }
 }
